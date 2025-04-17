bafb9fa6d95aeab889a1d485dff1d22df1bac779
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-04-17 16:19:14 -0400

    revert gold

--- models/gold/core/core__fact_transfers.sql
+++ models/gold/core/core__fact_transfers.sql
@@ -8,73 +8,33 @@
     tags = ['core','full_test']
 ) }}
 
-WITH silver_transfers AS (
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    version,
+    success,
+    event_index,
+    creation_number,
+    transfer_event,
+    account_address,
+    amount,
+    token_address,
+    transfers_id AS fact_transfers_id,
+    inserted_timestamp,
+    modified_timestamp
+FROM
+    {{ ref(
+        'silver__transfers'
+    ) }}
+WHERE
+    amount <> 0
+
+{% if is_incremental() %}
+AND modified_timestamp >= (
     SELECT
-        block_number,
-        block_timestamp,
-        tx_hash,
-        version,
-        success,
-        event_index,
-        creation_number,
-        transfer_event,
-        account_address,
-        amount,
-        token_address,
-        transfers_id AS fact_transfers_id,
-        inserted_timestamp,
-        modified_timestamp
+        MAX(modified_timestamp)
     FROM
-        {{ ref('silver__transfers') }}
-    WHERE
-        amount <> 0
-        {% if is_incremental() %}
-        AND modified_timestamp >= (
-            SELECT
-                MAX(modified_timestamp)
-            FROM
-                {{ this }}
-        )
-        {% endif %}
-),
-
-silver_transfers_usdt AS (
-    SELECT
-        block_number,
-        block_timestamp,
-        tx_hash,
-        version,
-        success,
-        event_index,
-        creation_number,
-        transfer_event,
-        account_address,
-        amount,
-        token_address,
-        transfers_id AS fact_transfers_id,
-        inserted_timestamp,
-        modified_timestamp
-    FROM
-        {{ ref('silver__transfers_usdt') }}
-    WHERE
-        amount <> 0
-        {% if is_incremental() %}
-        AND modified_timestamp >= (
-            SELECT
-                MAX(modified_timestamp)
-            FROM
-                {{ this }}
-        )
-        {% endif %}
-),
-
-combined_transfers AS (
-
-    SELECT * FROM silver_transfers
-    
-    UNION ALL
-    
-    SELECT * FROM silver_transfers_usdt
-)    
-
-SELECT * FROM combined_transfers
+        {{ this }}
+)
+{% endif %}
