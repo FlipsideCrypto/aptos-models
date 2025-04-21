deba3dac12b720043334018ef3bdd65a2325ca13
gregoriustanleyy <gstanleytejakusuma@gmail.com>
2025-04-22 00:07:32 +0700

    silver model

---
+++ models/silver/defi/dex/silver__dex_swaps_thala_v2.sql
@@ -0,0 +1,131 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "dex_swaps_thala_v2_id",
+    incremental_strategy = 'merge',
+    merge_exclude_columns = ["inserted_timestamp"],
+    cluster_by = ['modified_timestamp::DATE']
+) }}
+
+{% if execute %}
+
+{% if is_incremental() %}
+{% set max_mod_query %}
+
+SELECT 
+    COALESCE(MAX(modified_timestamp), '1970-01-01'::timestamp_ntz) as max_timestamp
+FROM 
+    {{ this }}
+
+{% endset %}
+{% set max_mod = run_query(max_mod_query).columns[0].values()[0] %}
+{{ log("Current max_mod timestamp: " ~ max_mod, info=True) }}
+
+{% endif %}
+{% endif %}
+
+WITH tx AS (
+    SELECT
+        tx_hash,
+        block_timestamp,
+        sender,
+        modified_timestamp
+    FROM
+        {{ ref('silver__transactions') }}
+    WHERE
+        success
+
+{% if is_incremental() %}
+AND modified_timestamp >= '{{max_mod}}'
+{% endif %}
+),
+events AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        version,
+        tx_hash,
+        event_index,
+        event_type,
+        event_address,
+        event_resource,
+        event_data,
+        modified_timestamp
+    FROM
+        {{ ref('silver__events') }}
+    WHERE
+        success
+        AND event_address = '0x7730cd28ee1cdc9e999336cbc430f99e7c44397c0aa77516f6f23a78559bb5'
+        AND event_resource LIKE 'SwapEvent%'
+
+{% if is_incremental() %}
+AND modified_timestamp >= '{{max_mod}}'
+{% endif %}
+), 
+joined AS (
+    SELECT
+        tx.tx_hash,
+        tx.block_timestamp,
+        tx.sender,
+        e.block_number,
+        e.version,
+        e.event_index,
+        e.event_type,
+        e.event_address,
+        e.event_resource,
+        e.event_data,
+        COALESCE(GREATEST(e.modified_timestamp, tx.modified_timestamp),  e.modified_timestamp, tx.modified_timestamp) as modified_timestamp
+    FROM 
+        tx
+        JOIN events e USING(
+            tx_hash, block_timestamp
+        )
+{% if is_incremental() %}
+WHERE
+    GREATEST(
+        tx.modified_timestamp,
+        e.modified_timestamp
+    ) >= '{{max_mod}}'
+{% endif %}
+),
+parsed AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        version,
+        tx_hash,
+        event_index,
+        event_address,
+        sender AS swapper,
+        event_data:idx_in :: INT AS idx_in,
+        event_data:idx_out :: INT AS idx_out,
+        event_data:amount_in :: INT AS amount_in,
+        event_data:amount_out :: INT AS amount_out,
+        event_data:metadata[event_data:idx_in :: INT]:inner :: STRING AS token_in,
+        event_data:metadata[event_data:idx_out :: INT]:inner :: STRING AS token_out,
+        modified_timestamp
+    FROM
+        joined
+    WHERE
+        event_data:idx_in IS NOT NULL
+        AND event_data:idx_out IS NOT NULL
+)
+SELECT
+    block_number,
+    block_timestamp,
+    version,
+    tx_hash,
+    event_index,
+    event_address,
+    swapper,
+    token_in,
+    token_out,
+    amount_in,
+    amount_out,
+    {{ dbt_utils.generate_surrogate_key(
+        ['tx_hash','event_index']
+    ) }} AS dex_swaps_thala_v2_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    parsed
