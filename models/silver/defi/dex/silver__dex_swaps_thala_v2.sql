3e8ef510f95a5aa7b03c693b0696fa4798acd2f8
gregoriustanleyy <gstanleytejakusuma@gmail.com>
2025-04-23 22:00:58 +0700

    changed inc logic

--- models/silver/defi/dex/silver__dex_swaps_thala_v2.sql
+++ models/silver/defi/dex/silver__dex_swaps_thala_v2.sql
@@ -11,15 +11,42 @@
 {% if is_incremental() %}
 {% set max_mod_query %}
 
-SELECT 
-    COALESCE(MAX(modified_timestamp), '1970-01-01'::timestamp_ntz) as max_timestamp
-FROM 
+SELECT
+    MAX(modified_timestamp) modified_timestamp
+FROM
     {{ this }}
 
-{% endset %}
-{% set max_mod = run_query(max_mod_query).columns[0].values()[0] %}
-{{ log("Current max_mod timestamp: " ~ max_mod, info=True) }}
+    {% endset %}
+    {% set max_mod = run_query(max_mod_query) [0] [0] %}
+    {% if not max_mod or max_mod == 'None' %}
+        {% set max_mod = '2099-01-01' %}
+    {% endif %}
 
+    {% set min_block_date_query %}
+SELECT
+    MIN(
+        block_timestamp :: DATE
+    )
+FROM
+    (
+        SELECT
+            MIN(block_timestamp) block_timestamp
+        FROM
+            {{ ref('silver__transactions') }} A
+        WHERE
+            modified_timestamp >= '{{max_mod}}'
+        UNION ALL
+        SELECT
+            MIN(block_timestamp) block_timestamp
+        FROM
+            {{ ref('silver__events') }} A
+        WHERE
+            modified_timestamp >= '{{max_mod}}'
+    ) {% endset %}
+    {% set min_bd = run_query(min_block_date_query) [0] [0] %}
+    {% if not min_bd or min_bd == 'None' %}
+        {% set min_bd = '2099-01-01' %}
+    {% endif %}
 {% endif %}
 {% endif %}
 
@@ -35,7 +62,7 @@ WITH tx AS (
         success
 
 {% if is_incremental() %}
-AND modified_timestamp >= '{{max_mod}}'
+AND block_timestamp :: DATE >= '{{min_bd}}'
 {% endif %}
 ),
 events AS (
@@ -58,7 +85,7 @@ events AS (
         AND event_resource LIKE 'SwapEvent%'
 
 {% if is_incremental() %}
-AND modified_timestamp >= '{{max_mod}}'
+AND block_timestamp :: DATE >= '{{min_bd}}'
 {% endif %}
 ), 
 joined AS (
@@ -80,11 +107,10 @@ joined AS (
             tx_hash, block_timestamp
         )
 {% if is_incremental() %}
-WHERE
-    GREATEST(
-        tx.modified_timestamp,
-        e.modified_timestamp
-    ) >= '{{max_mod}}'
+WHERE GREATEST(
+    tx.modified_timestamp,
+    e.modified_timestamp
+) >= '{{max_mod}}'
 {% endif %}
 ),
 parsed AS (
