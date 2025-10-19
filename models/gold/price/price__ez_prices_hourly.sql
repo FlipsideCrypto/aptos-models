4eb54a4f563d81a1894207b25f9f20f6cd4ff346
tarikceric <tarik@flipsidecrypto.com>
2025-10-19 13:01:21 -0500

    removed comment to add native

--- models/gold/price/price__ez_prices_hourly.sql
+++ models/gold/price/price__ez_prices_hourly.sql
@@ -21,7 +21,7 @@ SELECT
     hourly_prices_priority_id AS ez_prices_hourly_id
 FROM
     {{ ref('silver__hourly_prices_priority') }}
-    {# UNION ALL
+UNION ALL
 SELECT
     HOUR,
     token_address,
@@ -32,9 +32,10 @@ SELECT
     TRUE AS is_native,
     is_deprecated,
     is_imputed,
+    TRUE AS is_verified,
     inserted_timestamp,
     modified_timestamp,
     complete_native_prices_id AS ez_prices_hourly_id
 FROM
     {{ ref('silver__complete_native_prices') }}
-    #}
+
